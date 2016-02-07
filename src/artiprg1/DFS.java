fb738ec7b339acca58995ca5fccb18849a0a3b1d
Tsiniloiv <jonb12@ru.is>
2016-02-07 19:40:39 +0000

    more dfs work

--- src/artiprg1/DFS.java
+++ src/artiprg1/DFS.java
@@ -11,26 +11,27 @@ public class DFS implements Search {
 	 * TODO: Frontier, 
 	 */
 	private Queue<SearchNode> frontier = new LinkedList<SearchNode>();
-	private ArrayList<String> legalActions;
-	private Stack<String> solution;
+	private ArrayList<String> legalActions = new ArrayList<String>();
+	private Stack<String> solution = new Stack<String>();
 	private SearchNode current;
 	
 	public DFS(State initial) {
 		frontier.add(new SearchNode(initial, null, null));
 		if(search()) {
-			
+			System.out.println("Search failed!");
 		};
 	}
 	
 	public boolean search() {
 		while(true) {
-			current = frontier.poll();
-			if(current == null) {
+			if(frontier.isEmpty()) {
 				return false;
 			}
+			current = frontier.remove();
 			if(current.getState().isGoal()) {
 				//Trace back up and construct solution
 				for(SearchNode n = current; n != null; n = n.getParent()) {
+					System.out.println("Adding action to solution list");
 					solution.add(n.getAction());
 				}
 				return true;
@@ -54,6 +55,10 @@ public class DFS implements Search {
 
 	@Override
 	public String nextMove() {
+		if(solution.isEmpty()) {
+			System.out.println("NOW YOU FUCKED UP");
+			System.exit(-49);;
+		}
 		return solution.pop();
 	}
 }
